3dae6835316e9ffa2b3c9e1edae5ef1d43fd110b
Noob <ankita09singh54.4@gmail.com>
2026-09-18 20:11:19 +0530

    Create substitution_cipher_encryption_decryption.py

---
+++ substitution_cipher_encryption_decryption.py
@@ -0,0 +1,39 @@
+import sys
+
+value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+key = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
+   
+
+def substitution_encryption(text):
+    table = str.maketrans(value,key)
+    output =  text.translate(table)
+    return output
+
+def substitution_decryption(text):
+    table = str.maketrans(key,value)
+    output = text.translate(table)
+    return output
+
+def main():
+   while True :
+       
+        print("1.Encryption\n2.Decryption\n3.Exit")
+        usr_input = int(input("Choose a option:-"))
+
+        if usr_input == 1:
+            text = input("Enter the text to encrypt:- ")
+            print(substitution_encryption(text))
+        elif usr_input == 2:
+            text = input("Enter the text to decrypt:- ")
+            print(substitution_decryption(text))
+        elif usr_input == 3:
+            print ("Exiting....")
+            sys.exit
+        else:
+            print("Invalid Input")
+            sys.exit()
+            
+main()
+
+
+    
