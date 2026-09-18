01e0858dc3cdb24fdae106de5429cbb52b50e1ec
Noob <ankita09singh54.4@gmail.com>
2026-09-18 09:36:41 +0530

    Intial Commit

---
+++ hex_to_ascii.py
@@ -0,0 +1,24 @@
+def dirty_hex_to_string():
+
+    messy_hex = input("Enter the Messy Hex:- ")
+    clean_hex = messy_hex.replace(",","").replace("0x","")
+    ascii_string = bytes.fromhex(clean_hex).decode('ascii')
+    print(ascii_string)
+
+def clean_hex_to_string():
+
+    clean_hex = input("Enter the clean Hex:-")
+    ascii_string = bytes.fromhex(clean_hex).decode('ascii')
+    print(ascii_string)
+
+def main():
+    print(" 1.I have a messy hex(with 0x)\n 2.I have clean hex(without 0x)\n")
+    option = int(input("Choose a option:- "))
+    if(option == 1 ):
+        dirty_hex_to_string()
+    elif(option == 2):
+        clean_hex_to_string()
+    else:
+        print("Invalid option")
+
+main()        
