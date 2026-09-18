fa19a1d4ea7426a0d3057a6d0f72c5b61295b9a5
Noob <ankita09singh54.4@gmail.com>
2026-09-18 10:51:17 +0530

    Upgrade ROT13 to input based

--- ROT13.py
+++ ROT13.py
@@ -1,3 +1,18 @@
-A='z' 
-x=chr((ord(A)-ord('a') + 13)%26 + ord('a'))
-print(x)
+def ROT13(text):
+    result = ""
+    for char in text:
+        if char.isupper():
+            result += chr((ord(char)-ord('A')+13)%26 + ord('A'))
+        elif char.islower():
+            result += chr((ord(char)-ord('a')+13)%26 + ord('a'))   
+        else:
+            result += char   
+    print(result)          
+
+
+def main():
+    text= input("Enter the text or string at which you want to apply ROT13 :- ")
+    ROT13(text)
+
+
+main()
