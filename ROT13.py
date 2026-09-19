6ccf09cf1895fdc1ae390baa242b83f0ba20dfc4
Noob <ankita09singh54.4@gmail.com>
2026-09-19 10:04:48 +0530

    Added the encoder and decoder of ROT13

--- ROT13.py
+++ ROT13.py
@@ -1,3 +1,5 @@
+import sys
+
 def ROT13(text):
     result = ""
     for char in text:
@@ -7,12 +9,29 @@ def ROT13(text):
             result += chr((ord(char)-ord('a')+13)%26 + ord('a'))   
         else:
             result += char   
-    print(result)          
+    return result          
 
+
+def ROT13_encoder():
+    text= input("Enter the text or string you want to encode in ROT13 :- ")
+    print(ROT13(text))
+
+def ROT13_decoder():
+    text = input("Enter the text or string you want to decode in ROT13 :- ")
+    print(ROT13(ROT13(text)))
 
 def main():
-    text= input("Enter the text or string at which you want to apply ROT13 :- ")
-    ROT13(text)
-
+    while True:
+        print("1.Encode\n2.Decode\n3.Exit")
+        usr = input("Choose an option")
+        if usr == "1":
+            ROT13_encoder()
+        elif usr == "2":
+            ROT13_decoder()
+        elif usr == "3":
+            sys.exit()
+        else:
+            print("Invalid Input")
 
 main()
+                        
