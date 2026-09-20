639c727d97f4f693226a7bc6891c641db19862b9
Noob <ankita09singh54.4@gmail.com>
2026-09-20 20:26:45 +0530

    Implement Caesar Cipher functionality with user input for encryption and decryption

---
+++ Caeser_Cipher/caeser_cipher_without_mod.py
@@ -0,0 +1,44 @@
+import sys
+
+while True:
+    try:
+        k = int(input("Enter the integer between 0 and 25:- "))
+        if k < 0 or k > 25:
+            print("Invailid Input")
+        else:
+            print(k)
+            break
+    except ValueError:
+        print("Invalid data type")
+
+value = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+key = value[(k-1):25]+value[0:k]
+
+def caeser_cipher_encryption(text):
+    table = str.maketrans(value,key)
+    output = text.translate(table)
+    return output 
+def caeser_cipher_decryption(text):
+    table = str.maketrans(key,value)
+    output = text.translate(table)
+    return output
+
+def main():
+    while True:
+        print("1.Encryption\n2.Decryption\n3.Exit")
+        usr = input("Choose an option:- ")
+        if usr == "1":
+            text = input("Enter a string to encrypt:- ")
+            print(f" The Encrypted string is :- {caeser_cipher_encryption(text)}")
+
+        elif usr == "2":
+            text = input("Enter a string to decrypt:- ")
+            print(f"The decrypted string is :- {caeser_cipher_decryption(text)}")
+
+        elif usr == "3":
+            print("Exiting....")
+            sys.exit()
+        else:
+            print("Invalid option")  
+
+main() 
