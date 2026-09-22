b92cf20e640a6210ebcd4b455ca9727fe63a9f65
Noob <ankita09singh54.4@gmail.com>
2026-09-22 13:07:31 +0530

    Add the implementation to lower case

--- Caeser_Cipher/Caeser_cipher.py
+++ Caeser_Cipher/Caeser_cipher.py
@@ -13,9 +13,13 @@ while True:
 def caeser_cipher_encryption(text):
     result = "" 
     for char in text:
-        if char.isalpha():
+        if char.isupper():
             new_index = (ord(char) - ord('A') + usr )%26
             new_char = chr(ord('A') + new_index) 
+            result += new_char
+        elif char.islower():
+            new_index = (ord(char) - ord('a') + usr )%26
+            new_char = chr(ord('a') + new_index)
             result += new_char
         else:
             result += char
@@ -24,28 +28,33 @@ def caeser_cipher_encryption(text):
 def caeser_cipher_decryption(text):
     result = ""
     for char in text:
-        if char.isalpha():
+        if char.isupper():
             new_index = (ord(char) - ord('A') - usr)%26
             new_char = chr(ord('A') + new_index)
+            result += new_char
+        elif char.islower():
+            new_index = (ord(char) - ord('a') - usr)%26
+            new_char = chr(ord('a') + new_index)
             result += new_char
         else:
             result += char
     return result
         
 def main():
-    print("1.Encryption\n2.Decryption\n3.Exit")
-    user = input("Choose an option :-")
-    if user == "1":
-        text = input("Enter a string to encrypt:- ")
-        print(f"The Encrypted string is :- {caeser_cipher_encryption(text)}")
-    elif user == "2":
-        text = input("Enter a string to decrypt:- ")
-        print(f"The decrypted string is :- {caeser_cipher_decryption(text)}")
-    elif user == "3":
-        print("Exiting....")
-        sys.exit()
-    else:
-        print("Invalid option")
+    while True:
+        print("1.Encryption\n2.Decryption\n3.Exit")
+        user = input("Choose an option :-")
+        if user == "1":
+            text = input("Enter a string to encrypt:- ")
+            print(f"The Encrypted string is :- {caeser_cipher_encryption(text)}")
+        elif user == "2":
+            text = input("Enter a string to decrypt:- ")
+            print(f"The decrypted string is :- {caeser_cipher_decryption(text)}")
+        elif user == "3":
+            print("Exiting....")
+            sys.exit()
+        else:
+            print("Invalid option")
 
 main()
     
